79a1aaeaa5deb3bc99a803e890c8440d2a200c46
jk-kim113 <57799166+jk-kim113@users.noreply.github.com>
2019-12-04 21:39:32 +0900

    clicker window and animation

---
+++ Clicker/Assets/Scripts/MainUIController.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class MainUIController : MonoBehaviour
+{
+    private static int mUImovehash = Animator.StringToHash("Move");
+
+    [SerializeField]
+    Animator[] mWindowAims;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    public void MoveWindow(int id)
+    {
+        mWindowAims[id].SetTrigger(mUImovehash);
+    }
+}
